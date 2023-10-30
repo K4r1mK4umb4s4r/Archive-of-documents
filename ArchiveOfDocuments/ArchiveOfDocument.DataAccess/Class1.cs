3eea654e90b4f2efc6c021e3436a475c908d4e56
Lavlinsky Nickolay <kritikui24@gmail.com>
2023-10-30 21:16:40 +0300

    Laba_2

---
+++ ArchiveOfDocuments/ArchiveOfDocument.DataAccess/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ArchiveOfDocument.DataAccess
+{
+    public class Class1
+    {
+    }
+}
---
+++ ArchiveOfDocuments/ArchiveOfDocuments.WebAPI/loC/SerilogConfigurator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArchiveOfDocuments.WebAPI.loC
+{
+    public class SerilogConfigurator
+    {
+    }
+}
